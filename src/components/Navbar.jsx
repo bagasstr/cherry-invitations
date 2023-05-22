c533333854c00e913edd06ffd3084170189626c1
bagasstr <nervt.qng@gmail.com>
2023-05-22 10:59:37 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+'use client'
+import Image from 'next/image'
+import logo from '../assets/logo.png'
+import React, { useState } from 'react'
+import { TbMenu, TbX } from 'react-icons/tb'
+import Link from 'next/link'
+
+const Navbar = () => {
+  const [menu, setMenu] = useState(false)
+  return (
+    <>
+      <header className='fixed w-full'>
+        <div className='max-w-5xl mx-auto py-4 px-4'>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center'>
+              <Image
+                src={logo}
+                width={0}
+                priority={logo}
+                height={0}
+                alt='logo'
+                className='w-14 mr-2'
+              />
+              <h1 className='text-xl text-text leading-5 desktop:font-normal font-semibold'>
+                CHERRY
+                <span className='text-light text-accent'>
+                  <br />
+                  invitation
+                </span>
+              </h1>
+            </div>
+            <div className=''>
+              <div
+                className='desktop:hidden'
+                onClick={() => setMenu((prv) => !prv)}
+              >
+                {menu ? (
+                  <TbX className='text-2xl' />
+                ) : (
+                  <TbMenu className='text-2xl' />
+                )}
+              </div>
+              <ul
+                className={`desktop:flex desktop:gap-x-7 mobile:absolute tablet:absolute tablet:px-4 mobile:px-4 tablet:py-4 mobile:py-4 tablet:mt-8 mobile:mt-8 tablet:right-0 mobile:right-0 tablet:transition-all mobile:transition-all tablet:ease-in-out mobile:ease-in-out tablet:duration-200 mobile:duration-200 tablet:bg-accent2 mobile:bg-accent2 tablet:w-[50%] mobile:w-[50%] tablet:h-screen mobile:h-screen ${
+                  menu
+                    ? 'mobile:right-0 tablet:right-0'
+                    : 'mobile:right-[-1000px] tablet:right-[-1000px]'
+                }`}
+              >
+                <li className='text-text hover:text-accent font-light text-2xl'>
+                  <Link href={'/home'}>Home</Link>
+                </li>
+                <li className='text-text hover:text-accent font-light text-2xl'>
+                  <Link href={'/fitur'}>Fitur</Link>
+                </li>
+                <li className='text-text hover:text-accent font-light text-2xl'>
+                  <Link href={'/katalog'}>Katalog</Link>
+                </li>
+                <li className='text-text hover:text-accent font-light text-2xl'>
+                  <Link href={'/harga'}>Harga</Link>
+                </li>
+                <li className='text-text hover:text-accent font-light text-2xl'>
+                  <Link href={'/faq'}>Faq</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/(pages)/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        mobile: { min: '300px', max: '767px' },
+        //   mobile: '300px',
+        tablet: { min: '768px', max: '1023px' },
+        desktop: '1024px',
+      },
+
+      colors: {
+        bg: '#F6F1F1',
+        text: '#27374D',
+        accent: '#1189f7',
+        accent2: '#dbedff',
       },
     },
   },
